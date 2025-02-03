8ce2131142eb6489ea4a01c12146fbd5d1352a17
mgonrui <mariogonrui@protonmail.com>
2025-02-03 15:09:05 +0100

    clean code

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -20,7 +20,7 @@
 ;; KEY MAPPING ENDS
 
 ;; USER INTERFACE STARTS
-;;set fonts
+;; set fonts
 ;; See 'C-h v doom-font' for documentation and more examples of what they accept
 (setq doom-font (font-spec :family "Agave Nerd Font" :size 20 :weight 'semi-light);; the primary font to use
       doom-variable-pitch-font (font-spec :family "Agave Nerd Font" :size 15) ;; a non-monospace font (where applicable)
@@ -65,6 +65,13 @@ select-window-6
 select-window-7
 select-window-8
 select-window-9)))
+;; disable autocompletion in .org, .txt, .md
+(defun disable-company-mode ()
+  (company-mode -1))
+(add-hook 'markdown-mode-hook 'disable-company-mode)
+(add-hook 'org-mode-hook 'disable-company-mode)
+(add-hook 'text-mode-hook 'disable-company-mode)
+(add-hook 'c-mode-hook 'disable-company-mode)
 ;; USER INTERFACE ENDS
 
 ;; LSP STARTS
@@ -82,7 +89,9 @@ select-window-9)))
 ;; set doom dir
 (setq doom-user-dir "/home/mgr/.dotfiles/.config/doom/")
 ;; load org configuration
-(load-file "~/.config/doom/org.el")
+(load! "~/.config/doom/org.el")
+;; load libraries folder
+(add-load-path! "libs")
 ;; System locale to use for formatting time values.
 (setq system-time-locale "C")         ; Make sure that the weekdays in the time stamps of
                                       ; your Org mode files and in the agenda appear in English.
@@ -103,58 +112,13 @@ select-window-9)))
   (setq compile-command(concat "make  && ./a.out | cat -e" ))
   (compile compile-command))
 ;; column marker
-(add-to-list 'load-path "~/.config/doom/libs/column-marker")
-(load-file "~/.config/doom/libs/column-marker/column-marker.el")
+(add-load-path! "libs/column-marker")
+(load! "~/.config/doom/libs/column-marker/column-marker.el")
 (add-hook 'c-mode-hook (lambda () (interactive) (column-marker-1 80))) ;; mark a line if its more than 80 col long
 ;; insert 42  header plugin
-(add-to-list 'load-path "~/.config/doom/libs")
-(add-to-list 'load-path "~/.config/doom/libs/42header_emacs")
-(load-file "~/.config/doom/libs/42header_emacs/comments.el")
-(load-file "~/.config/doom/libs/42header_emacs/header.el")
-(load-file "~/.config/doom/libs/42header_emacs/init.el")
-(load-file "~/.config/doom/libs/42header_emacs/list.el")
-(load-file "~/.config/doom/libs/42header_emacs/string.el")
-;; C PROGRAMMING ENDS
-
-;; Whenever you reconfigure a package, make sure to wrap your config in an
-;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
-;;
-;;   (after! PACKAGE
-;;     (setq x y))
-;;
-;; The exceptions to this rule:
-;;
-;;   - Setting file/directory variables (like `org-directory')
-;;   - Setting variables which explicitly tell you to set them before their
-;;     package is loaded (see 'C-h v VARIABLE' to look up their documentation).
-;;   - Setting doom variables (which start with 'doom-' or '+').
-;;
-;; Here are some additional functions/macros that will help you configure Doom.
-;;
-;; - `load!' for loading external *.el files relative to this one
-;; - `use-package!' for configuring packages
-;; - `after!' for running code after a package has loaded
-;; - `add-load-path!' for adding directories to the `load-path', relative to
-;;   this file. Emacs searches the `load-path' when you load packages with
-;;   `require' or `use-package'.
-;; - `map!' for binding new keys
-;;
-;; To get information about any of these functions/macros, move the cursor over
-;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
-;; This will open documentation for it, including demos of how they are used.
-;; Alternatively, use `C-h o' to look up a symbol (functions, variables, faces,
-;; etc).
-;;
-;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
-;; they are implemented.
-;;
-;; disable autocompletion in .org, .txt, .md
-(defun disable-company-mode ()
-  (company-mode -1))
-(add-hook 'markdown-mode-hook 'disable-company-mode)
-(add-hook 'org-mode-hook 'disable-company-mode)
-(add-hook 'text-mode-hook 'disable-company-mode)
-(add-hook 'c-mode-hook 'disable-company-mode)
-
-
-(setq vterm-use-vterm-prompt-detection-method t)
+(add-load-path! "libs/42header_emacs")
+(load! "libs/42header_emacs/comments.el")
+(load! "libs/42header_emacs/header.el")
+(load! "libs/42header_emacs/init.el")
+(load! "libs/42header_emacs/list.el")
+(load! "libs/42header_emacs/string.el")
